7e238290dfb0e14fe97f9dd991ef65e10e834c6d
HangNT <HangNT169@fpt.edu.vn>
2024-05-06 15:03:25 +0700

    Buổi 1: Helloworld

---
+++ src/B1_Helloworld/LuyenTapBuoi1.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package B1_Helloworld;
+
+/**
+ *
+ * @author hangnt
+ */
+public class LuyenTapBuoi1 {
+
+    // B3: Tinh diem trung binh cua diem toan , 
+    // ly, hoa voi he so cua toan la 2, ly la 2 hoa la 1
+    public static void main(String[] args) {
+        int toan = 7;
+        int ly = 6;
+        int hoa = 8;
+        float dtb = (float) ((toan * 2 + ly * 2 + hoa) / 5);
+        System.out.println("dtb la" + dtb);
+    }
+}
---
+++ src/B1_Helloworld/LyThuyetBuoi1.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package B1_Helloworld;
+
+/**
+ *
+ * @author hangnt
+ */
+public class LyThuyetBuoi1 {
+
+    public static void main(String[] args) {
+//        System.out.println("Hang");
+//        System.out.println("123");
+//        System.out.println("gfds");
+//        System.out.println("sgfd");
+//        System.out.println("sgf");
+//        int[] c = {1, 2, 3, 4, 5};
+//        System.out.println(d[100]);
+        // Khai bao bien: kieu du lieu tenBien;
+//        int number = 5;
+//        int numberSecond = 6;
+//        System.out.println("Number = " + number);
+//        System.out.println("Number 2 = " + numberSecond);
+////        System.out.println("So la :" + number);
+        // In thong tin: Ten, tuoi, dia chi, ky hoc, nganh hoc, ten nyc cua ban than
+        // B1: Tinh tong tich thuong cua 2 so nguyen bat ky 
+        // B2: In thong tin ca nhan cua ban than gom ten, tuoi, dia chi
+        // B3: Tinh diem trung binh cua diem toan , ly, hoa voi he so cua toan la 2, ly la 2 hoa la 1
+        // B4: Tinh chu vi, dien tich hinh tron va hinh chu nhat voi gia tri bat ky 
+        // B6: In 2 số nguyên có giá trị khởi tạo tuỳ ý 
+        // B7: In ra tuổi khi có năm sinh là giá trị khởi tạo tuỳ ý 
+        
+        
+        int a = 3;
+        int b = 4;
+        int tong = a + b;
+        int hieu = a - b;
+        int tich = a * b;
+        float thuong = (float) a / b;
+        System.out.println("tong la:"+tong);
+        System.out.println("hieu la:"+hieu);
+        System.out.println("tich la:"+tich);
+        System.out.println("thuong la:"+thuong);
+        
+        
+
+    }
+
+}
