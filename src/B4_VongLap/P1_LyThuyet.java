b2842e7f7b703917418d54bb961e64cbadf7708d
HangNT <HangNT169@fpt.edu.vn>
2024-05-13 13:27:10 +0700

    Buổi 4: Vòng lặp

---
+++ src/B4_VongLap/P1_LyThuyet.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package B4_VongLap;
+
+/**
+ *
+ * @author hangnt
+ */
+public class P1_LyThuyet {
+    // Vong lap 
+    // C: for, while, do..while 
+    // Java: for i(for giong C),foreach, foreach + lamda
+    // foreach + method reference 
+    // Cu phap 
+    // for:
+    // for(gia tri khoi tao;dieu kien; buoc nhay){
+        // code 
+    // }
+    // while
+//    while(dieu kien){
+//    
+//    }
+    // do..while
+//    do{
+//        // code
+//    }while(dieu kien);
+    // Tao 1 menu gom cac chuc nang sau: Menu lap lai toi khi chon chuc nang thoat
+    //      1. Nhap thong tin ca nhan cua ban than va in ra man hinh
+    //      2. Tinh diem trung binh cua ky hoc vua roi cua ban than khi biet cac dau diem tuong ung
+    //      3. Nhập vào tên , tuổi, cân nặng, chiều cao, địa chỉ, kỳ học, chuyên ngành của bản thân và in ra màn hình
+    //      4. Tinh chi so BMI cua ban than 
+    //      5. Tinh P, S cua HCN, HV, HT 
+    //      6. Tim so lon nhat trong 3 so nguyen nhap tu ban phim 
+    //      7. Nhập n. Nhập đến khi là số nguyên dương. Xong tính tích từ 1 tới n 
+    //      8. Nhập vào 2 số nguyên. Tính tổng hiệu tích thương của 2 số đó 
+    //      9. Nhập vào 1 số. Kiểm tra nó thuộc tháng nào trong năm 
+    //      10. Nhập 3 số dương. Nhập đi nhập lại nếu không phải số dương. Kiểm tra xem 3 số đấy có hợp thành 1 tam giác hay không? Nếu có thì đó là tam giác loại gì 
+    //      11. Nhập vào n. Tính tổng các số lẻ của nghịch đảo: 1+1/3+..+1/n 
+    //      12. Đếm tất cả các số chẵn từ 1 tới n với n là số nhập từ bàn phím 
+    //      13.Tính tích của tất cả các số lẻ và chia hết cho 3 từ 1 - n  với n nhập vào từ bàn phím. 
+    //      14. Đếm tất cả các số lẻ và chia hết cho 5 từ 0 - n với n nhập vào từ bàn phím 
+    //      15. Liệt kê tất cả các số chẵn và chia hết cho 2 và 3 từ 0 - n với n nhập vào từ bàn  phím. 
+    //      16. Thoat
+}
